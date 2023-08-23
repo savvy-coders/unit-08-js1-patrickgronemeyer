ff226a7b5f4d9da75f4f206bee292632e92fb438
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-08-23 15:39:10 -0500

    correct logic in failing test

--- spec/js1Test8.spec.js
+++ spec/js1Test8.spec.js
@@ -1,8 +1,13 @@
-import {getIsAdult} from '../index.js';
+import {getAge, getIsAdult} from '../index.js';
 
 describe('JavaScript Coding Challenge', () => {
 
   it('should declare a variable named isAdult using the ternary operator, holding a string value of Adult if age is 18 or over, and Not Adult if age is under 18', () => {
-    expect(getIsAdult()).toBe(getIsAdult() >= 18 ? 'Adult' : 'Not Adult');
+    if(getAge() >= 18){
+      expect(getIsAdult()).toBe('Adult');
+    }else{
+      expect(getIsAdult()).toBe('Not Adult');
+    }
   });
 });
+
